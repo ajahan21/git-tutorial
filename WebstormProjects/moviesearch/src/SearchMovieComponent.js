b523684c87e1811563bf817e93253d5bfd61016c
Arisha Jahan <ajahan@worcester.edu>
2020-12-29 23:46:36 -0800

    managed state with react useState Hook

--- WebstormProjects/moviesearch/src/SearchMovieComponent.js
+++ WebstormProjects/moviesearch/src/SearchMovieComponent.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function searchMovie(){
+    //static -input query, movies <-- need to tract
+    //useState to manage data
+
+    //sending default valsue in usestate funcation
+    const [query, setQuery] = useState('');
     const searchMovie = async (e) => {
         e.preventDefault();
         console.log("submitting");
 
         const searchMovies = async (e) => {
             e.preventDefault();
-            console.log("submitting");
-
-            const query = "Jurassic Park";
 
             const url = 'https://api.themoviedb.org/3/search/movie?api_key=d11bdf4d11e25cd7375edacc7c44f744&language=en-US&query=${query}&page=1&include_adult=face';
             try {
@@ -27,7 +29,8 @@ export default function searchMovie(){
             </label>
 
             <input className="input" type="text"  name="query"
-                    placeholder="i.e Jurassic Park"/>
+                    placeholder="i.e Jurassic Park"
+                    value={query} onChange={(e) => setQuery(e.target.value)}/>
             <button className="button" type="submit"> Search </button>
         </form>
     )
